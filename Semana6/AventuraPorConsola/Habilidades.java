611a4cd09b06511355ed4cba6041c7ca74812ac8
c4rlos19450 <infocarlosexpuig@gmail.com>
2023-05-23 02:09:22 +0200

    Creado

---
+++ Semana6/AventuraPorConsola/Habilidades.java
@@ -0,0 +1,4 @@
+package AventuraPorConsola;
+
+public class Habilidades {
+}
---
+++ Semana6/AventuraPorConsola/Persona.java
@@ -0,0 +1,10 @@
+package AventuraPorConsola;
+
+import HundirLaFlota.Coordenadas;
+
+import java.util.ArrayList;
+
+public abstract class Persona {
+    protected ArrayList<Habilidades> listaDeHabilidades = new ArrayList<Habilidades>();
+    protected ArrayList<Equipo> listaDeObjetosEnElEquipo = new ArrayList<Equipo>();
+}
